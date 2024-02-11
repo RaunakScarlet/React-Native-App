96a3d638b1b8d7b0d729c2577406f619c482250c
Raunak Scarlet <raunakscarlet@gmail.com>
2024-02-11 16:29:19 +0530

    used imgae & imagebackground

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-const { View,Text } = require("react-native");
-
+const { View,Text,Image,ImageBackground } = require("react-native");
+const logo = require("./assets/adaptive-icon.png");
 export default function App() {
     return (
-        <View style={{ flex: 1, backgroundColor: "green", padding: 60 }}>
+        <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
             <Text>
                 hello
-                <Text style={{color:"yellow"}}>world!</Text> how r u ?{" "}
+                <Text style={{ color: "yellow" }}>world!</Text> how r u ?{" "}
             </Text>
+            <Image source={logo} style={{ width: 300, height: 300 }} />
+            <ImageBackground style={{flex:1, width: 300, height: 300 }}
+                source={{uri:"https://picsum.photos/300"}}>
+                <Text>hello</Text>
+            </ImageBackground>
         </View>
     );
 }
