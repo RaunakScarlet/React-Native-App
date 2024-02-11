6374641029ca51811d05cae16393ecd65f466534
Raunak Scarlet <raunakscarlet@gmail.com>
2024-02-11 15:43:04 +0530

    used text

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
-const { View } = require("react-native");
+const { View,Text } = require("react-native");
 
 export default function App() {
-    return <View style={{ flex: 1, backgroundColor: "green" }}></View>;
+    return (
+        <View style={{ flex: 1, backgroundColor: "green", padding: 60 }}>
+            <Text>
+                hello
+                <Text style={{color:"yellow"}}>world!</Text> how r u ?{" "}
+            </Text>
+        </View>
+    );
 }
